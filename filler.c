9f6e2cf62b0e101dccf7dd396058bdf6ef6239d1
Yixin LI <yli@e1z2r8p24.42.us.org>
2017-06-20 16:37:29 -0700

    saving info, will work on saving map

--- filler.c
+++ filler.c
@@ -10,12 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "includes/filler.h"
+#include "src/libft/libft.h"
 
-int main()
+int	main()
 {
-	printf("3 3\n");
+	t_info info_stru;
+	char	*buf;
+	char	**string;
+	int		row;
+	int		col;
+	int		p;
+
+	ft_memset(&info_stru, 0, sizeof(info_stru));
+	while (get_next_line(0, &buf) > 0)
+	{
+		if (ft_strnequ(buf, "$$$ exec ", 9))
+		{
+			p = ft_atoi(&buf[10]);
+			info_stru.plr = p;
+			dprintf(2, "|player is %d|\n", info_stru.plr);
+		}
+		if (ft_strnequ(buf, "Plateau ", 8))
+		{
+			string = ft_strsplit(buf, ' ');
+			row = ft_atoi(string[1]);
+			col = ft_atoi(string[2]);
+			dprintf(2, "|row is %d|\n", row);
+			dprintf(2, "|col is %d|\n", col);
+		}
+		//ft_putstr_fd(buf, 2);
+		//dprintf(2, "\n");
+		//return (0);
+	}
+	printf("8 2");
 	return (0);
 }
---
+++ includes/filler.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yli <yli@student.42.us.org>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/19 16:05:48 by yli               #+#    #+#             */
+/*   Updated: 2017/06/19 16:05:49 by yli              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLER_H
+# define FILLER_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+
+
+# include <stdio.h>
+
+
+typedef struct s_info
+{
+  int plr;
+  int row;
+  int col;
+}       t_info;
+
+#endif
